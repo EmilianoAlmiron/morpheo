b4f3baa9a34c2aad60451f35a5bff061904b4ba1
Emiliano <almironemiliano@gmail.com>
2021-07-12 22:57:35 -0300

    agregamos nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import NavBar from './components/NavBar';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <TopNav/>
     </div>
   );
 }
